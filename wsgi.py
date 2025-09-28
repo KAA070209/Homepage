8d8a3b35797b2f16983f052567ecd22241900185
unknown <m.azkanabhan07@gmail.com>
2025-09-28 18:31:27 +0700

    fix: gunakan dotenv untuk config

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,11 @@ from app import app as application
 from dotenv import load_dotenv
 import os
 
-# load .env file
-load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+# Load environment variables dari file .env
+load_dotenv(".env")
+
+# Set Flask config dari env
+application.secret_key = os.getenv("SECRET_KEY")
+application.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+application.config["PREFERRED_URL_SCHEME"] = os.getenv("PREFERRED_URL_SCHEME", "https")
+application.config["SESSION_COOKIE_SECURE"] = os.getenv("SESSION_COOKIE_SECURE", "True") == "True"
